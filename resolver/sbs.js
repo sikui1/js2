be6ed2db851c7cd20f883aa641a8af889aec536e
superman <superman@gmail.com>
2022-11-23 01:35:39 +0700

    init

---
+++ resolver/sbs.js
@@ -0,0 +1,6 @@
+{
+    "cooke_key":"watchsb",
+    "cooke_value":"sbstream",
+    "start_value":"/sources48/655a68436d787363677849537c7c",
+    "end_value":"7c7c47634a755a494f64766856347c7c73747265616d7362"
+  }
---
+++ resolver/utb.js
@@ -0,0 +1,6 @@
+{
+    "getlink": "/api/streaming/source/get?token=%s&file_code=%s",
+    "token": "null",
+    "tokenlink": "/api/checkIp?token=",
+    "usedata": "/0/"
+  }
