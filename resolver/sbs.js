54aa05b4254aaafbbd44626411a00261625415b0
superman <superman@gmail.com>
2022-12-28 11:47:25 +0700

    test

--- resolver/sbs.js
+++ resolver/sbs.js
@@ -1,6 +1,6 @@
 {
     "cooke_key":"watchsb",
     "cooke_value":"sbstream",
-    "start_value":"/sources48/655a68436d787363677849537c7c",
-    "end_value":"7c7c47634a755a494f64766856347c7c73747265616d7362"
+    "start_value":"/sources48/394f724a6d6c52686934754f7c7c",
+    "end_value":"7c7c7341575470413451434170567c7c73747265616d7362"
   }
