68cecf744438c74572a2da56b300fec116c81766
superman <superman@gmail.com>
2023-03-13 16:21:08 +0700

    add

--- resolver/rbit.js
+++ resolver/rbit.js
@@ -1,6 +1,1 @@
-{
-    "cooke_key":"watchsb",
-    "cooke_value":"sbstream",
-    "start_value":"/sources15/6d6144797752744a454267617c7c",
-    "end_value":"7c7c7341575470413451434170567c7c73747265616d7362"
-  }
+c7d5237109f5abbe21aebe0185edd03b
