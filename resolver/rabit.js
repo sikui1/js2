b42c6128024805b1093a45480c9f96a61641a866
superman <superman@gmail.com>
2023-08-17 12:40:03 +0700

    add

---
+++ resolver/rabit.js
@@ -0,0 +1,14 @@
+[
+  {
+    "ver": "4",
+    "url": "https://raw.githubusercontent.com/enimax-anime/key/e4/key.txt",
+    "embed":"ajax/embed-4",
+    "mainurl": "https://rabbitstream.net"
+  },
+  {
+    "ver": "6",
+    "url": "https://raw.githubusercontent.com/enimax-anime/key/e4/key.txt",
+    "embed":"embed-2/ajax/e-1",
+    "mainurl": "https://megacloud.tv"
+  }
+]
