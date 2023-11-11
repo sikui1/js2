fef4747fff4f9eac60e8ba044e59a14357faa84e
superman <superman@gmail.com>
2023-11-11 14:29:06 +0700

    add

--- resolver/rabit.js
+++ resolver/rabit.js
@@ -1,13 +1,13 @@
 [
   {
     "ver": "4",
-    "url": "https://raw.githubusercontent.com/enimax-anime/key/e4/key.txt",
+    "url": "https://raw.githubusercontent.com/theonlymo/keys/e4/key",
     "embed":"ajax/embed-4",
     "mainurl": "https://rabbitstream.net"
   },
   {
     "ver": "6",
-    "url": "https://raw.githubusercontent.com/enimax-anime/key/e4/key.txt",
+    "url": "https://raw.githubusercontent.com/theonlymo/keys/e1/key",
     "embed":"embed-2/ajax/e-1",
     "mainurl": "https://megacloud.tv"
   }
