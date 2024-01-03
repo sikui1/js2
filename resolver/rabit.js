d25e7041b162f595ded4c5ec3e97f22901bc0675
superman <superman@gmail.com>
2024-01-03 15:07:47 +0700

    add

--- resolver/rabit.js
+++ resolver/rabit.js
@@ -1,13 +1,13 @@
 [
   {
     "ver": "4",
-    "url": "https://raw.githubusercontent.com/theonlymo/keys/e4/key",
+    "url": "https://raw.githubusercontent.com/Claudemirovsky/keys/e1/key",
     "embed":"ajax/embed-4",
     "mainurl": "https://rabbitstream.net"
   },
   {
     "ver": "6",
-    "url": "https://raw.githubusercontent.com/theonlymo/keys/e1/key",
+    "url": "https://raw.githubusercontent.com/Claudemirovsky/keys/e4/key",
     "embed":"embed-2/ajax/e-1",
     "mainurl": "https://megacloud.tv"
   }
