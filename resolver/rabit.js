67fff1355659c53bd2b3c45cb5ff3cd4317a1c6d
superman <superman@gmail.com>
2024-05-10 22:28:53 +0700

    init

--- resolver/rabit.js
+++ resolver/rabit.js
@@ -4,7 +4,8 @@
     "url": "https://raw.githubusercontent.com/eatmynerds/key/e4/key.txt",
     "embed":"ajax/v2/embed-4",
     "mainurl": "https://rabbitstream.net",
-    "exkey" : "&v=30061&h=cee6762ddfab4706a1045c519fdbc047ed85a4fe&b=1878522368",
+    "exkey" : "&v=30061&h=cee6762ddfab4706a1045c519fdbc047ed85a4fe&b=1878522368",,
+    "referer": "https://flixhq.to",
     "encrypted": true
   },
   {
@@ -12,6 +13,7 @@
     "url": "https://megacloud.tv/js/player/a/prod/e1-player.min.js",
     "embed":"embed-1/ajax/e-1",
     "exkey" : "",
+    "referer": "",
     "mainurl": "https://megacloud.tv",
     "encrypted": false
   }
