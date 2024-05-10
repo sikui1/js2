b97c5f97670d6aa4c5d25bba04be621a743f1843
superman <superman@gmail.com>
2024-05-10 22:15:42 +0700

    init

--- resolver/rabit.js
+++ resolver/rabit.js
@@ -2,14 +2,16 @@
   {
     "ver": "4",
     "url": "https://raw.githubusercontent.com/eatmynerds/key/e4/key.txt",
-    "embed":"ajax/embed-4",
+    "embed":"ajax/v2/embed-4",
     "mainurl": "https://rabbitstream.net",
+    "exkey" : "&v=30061&h=cee6762ddfab4706a1045c519fdbc047ed85a4fe&b=1878522368",
     "encrypted": true
   },
   {
     "ver": "6",
     "url": "https://megacloud.tv/js/player/a/prod/e1-player.min.js",
     "embed":"embed-1/ajax/e-1",
+    "exkey" : "",
     "mainurl": "https://megacloud.tv",
     "encrypted": false
   }
