7c5ae11cc4622a0e53c9bc0a8f9d69e138f5fff1
superman <superman@gmail.com>
2024-06-11 13:01:19 +0700

    + add

--- resolver/rabit.js
+++ resolver/rabit.js
@@ -1,11 +1,20 @@
 [
   {
     "ver": "6",
-    "url": "https://megacloud.tv/js/player/a/prod/e1-player.min.js",
-    "embed":"embed-1/ajax/e-1",
+    "url": "",
+    "embed":"embed-1/ajax/e-1/getSources?id=",
     "exkey" : "",
     "referer": "",
     "mainurl": "https://megacloud.tv",
+    "encrypted": false
+  },
+  {
+    "ver": "4",
+    "url": "",
+    "embed":"rabbit?id=",
+    "exkey" : "",
+    "referer": "",
+    "mainurl": "https://aquariumtv.app",
     "encrypted": false
   }
 ]
