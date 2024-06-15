d7b9d08ddee6d960cbb4579a1a42acd3c68715f8
superman <superman@gmail.com>
2024-06-15 10:19:26 +0700

    init

--- resolver/rabit.js
+++ resolver/rabit.js
@@ -21,7 +21,7 @@
     "ver": "5",
     "url": "",
     "embed":"ajax/v2/embed-4/getSources?id=",
-    "exkey" : "",
+    "exkey" : "&v=11399&h=961813e89f77c55a03e254fec9cbc1d63ef40c79&b=1878522368",
     "referer": "",
     "mainurl": "https://rabbitstream.net",
     "encrypted": true
