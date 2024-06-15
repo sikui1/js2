cb6aed16884addabce554e750e49cd4f94a15f04
superman <superman@gmail.com>
2024-06-15 18:33:37 +0700

    init

--- resolver/rabit.js
+++ resolver/rabit.js
@@ -16,14 +16,5 @@
     "referer": "https://fmovies.ps/",
     "mainurl": "https://aquariumtv.app",
     "encrypted": false
-  },
-  {
-    "ver": "5",
-    "url": "[122,97,159,109,93,175,50,197,197,253,130,29,177,117,237,146]",
-    "embed":"ajax/v2/embed-4/getSources?id=",
-    "exkey" : "&v=23560&h=349559d6df55ee823b96138b6653d72e9564b6b7&b=1676800512",
-    "referer": "",
-    "mainurl": "https://rabbitstream.net",
-    "encrypted": true
   }
 ]
--- resolver/rbit.js
+++ resolver/rbit.js
@@ -1,1 +1,11 @@
-c7d5237109f5abbe21aebe0185edd03b
+[
+    {
+        "ver": "5",
+        "url": "[122,97,159,109,93,175,50,197,197,253,130,29,177,117,237,146]",
+        "embed":"ajax/v2/embed-4/getSources?id=",
+        "exkey" : "&v=23560&h=349559d6df55ee823b96138b6653d72e9564b6b7&b=1676800512",
+        "referer": "https://flixhq.to/",
+        "mainurl": "https://rabbitstream.net",
+        "encrypted": true
+      }
+]
