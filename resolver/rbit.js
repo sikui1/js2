418841a6b0ebd1ee03b34f4652e7889a553f058f
superman <superman@gmail.com>
2024-06-18 11:39:44 +0700

    init

--- resolver/rbit.js
+++ resolver/rbit.js
@@ -7,5 +7,14 @@
         "referer": "https://flixhq.to/",
         "mainurl": "https://rabbitstream.net",
         "encrypted": true
+      },
+      {
+        "ver": "4",
+        "url": "https://megacloud.tv/js/player/a/prod/e1-player.min.js?v=",
+        "embed":"embed-2/ajax/e-1/getSources?id=",
+        "exkey" : "",
+        "referer": "",
+        "mainurl": "https://megacloud.tv",
+        "encrypted": true
       }
 ]
